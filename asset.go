3db7deffbde13cc1e9ad499cd0eea918f8cfe0bf
Saeed Rasooli <saeed.gnu@gmail.com>
2025-10-09 23:40:11 +0330

    add suffix and prefix to asset price

--- asset.go
+++ asset.go
@@ -7,17 +7,23 @@ import (
 )
 
 type Asset struct {
-	Name string `toml:"name"`
-	ID   string `toml:"id"`
+	Name   string `toml:"name"`
+	ID     string `toml:"id"`
+	Prefix string `toml:"prefix"`
+	Suffix string `toml:"suffix"`
 
 	Digits int `toml:"digits"`
 
 	HumanizeFormat string `toml:"humanize_format"` // go-humanize format, used if digits < 0
 }
 
-func (a *Asset) FormatPrice(price float64) string {
+func (a *Asset) numToStr(price float64) string {
 	if a.Digits < 0 {
 		return humanize.FormatFloat(a.HumanizeFormat, price)
 	}
 	return fmt.Sprintf("%.*f", a.Digits, price)
+}
+
+func (a *Asset) FormatPrice(price float64) string {
+	return a.Prefix + a.numToStr(price) + a.Suffix
 }
